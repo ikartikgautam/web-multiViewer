ce6fe6cf330dcdd0d7ec3d222ea97d308c9bf76d
thiskartik <gautam.kartik05@gmail.com>
2020-09-23 18:35:10 +0530

    image viewer added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { ImageViewerComponent } from './image-viewer/image-viewer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    TopBarComponent
+    TopBarComponent,
+    ImageViewerComponent
   ],
   imports: [
     BrowserModule,
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  type
+  src
+  yid
+
+  constructor(public para: ActivatedRoute) {
+
+    // Extract query parameters from url
+    this.para.queryParams.subscribe(params => {
+      this.type = params['type']
+      this.src = params['src']
+      this.yid = params['yid']
+    });
+
+  }
 
   ngOnInit(): void {
+
+    console.log(this.type)
+    console.log(this.src)
+    console.log(this.yid)
+
   }
 
 }
+
+/* =================
+
+MANUAL FOR PARAMETERS
+
+type = [img,vdo,yvdo,pdf,doc,excel,*rtf]
+src = [www.xyz.com]   //link to resource
+yid = [xyz123]    //video id in case of youtube type
+
+================= */
